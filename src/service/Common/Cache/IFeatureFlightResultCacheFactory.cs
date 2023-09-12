92761add4cb15844338736ba7f816c05c559ac81
“Rahas <rakhora@microsoft.com>
2023-09-12 14:09:24 +0530

    flighting result caching based on config

---
+++ src/service/Common/Cache/IFeatureFlightResultCacheFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.FeatureFlighting.Common.Caching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.FeatureFlighting.Common.Cache
+{
+    public interface IFeatureFlightResultCacheFactory
+    {
+        /// <summary>
+        /// Creates <see cref="ICache"/> based on the tenant and operation that has to be performed
+        /// </summary>
+        /// <param name="tenant">Tenant Name</param>
+        /// <param name="operation">Operation for which cache is required</param>
+        /// <param name="correlationId">Correlation ID of the operation</param>
+        /// <param name="transactionId">Transaction ID of the operation</param>
+        /// <returns cref="ICache">Cache</returns>
+        ICache Create(string tenant, string operation, string correlationId, string transactionId);
+    }
+}
---
+++ src/service/Infrastructure/Cache/FeatureFlightResultCacheFactory.cs
@@ -0,0 +1,41 @@
+﻿using AppInsights.EnterpriseTelemetry;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureFlighting.Common.Cache;
+using Microsoft.FeatureFlighting.Common.Caching;
+using Microsoft.FeatureFlighting.Common.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.FeatureFlighting.Infrastructure.Cache
+{
+    public class FeatureFlightResultCacheFactory : IFeatureFlightResultCacheFactory
+    {
+        private readonly IMemoryCache _memoryCache;
+        private readonly ILogger _logger;
+        private static readonly Dictionary<string, ICache> _cacheContainer = new();
+        private static readonly object _lock = new();
+
+        public FeatureFlightResultCacheFactory(IMemoryCache memoryCache, ILogger logger)
+        {
+            _memoryCache = memoryCache;
+            _logger = logger;
+        }
+
+        ICache IFeatureFlightResultCacheFactory.Create(string tenant, string operation, string correlationId, string transactionId)
+        {
+            lock (_lock)
+            {
+                var cacheContainerKey = $"{tenant.ToUpperInvariant()}:{operation.ToUpperInvariant()}";
+                if (_cacheContainer.Keys.Contains(cacheContainerKey))
+                    return _cacheContainer[cacheContainerKey];
+
+                ICache cache = new InMemoryCache(_memoryCache, tenant, _logger);
+                _cacheContainer.Add(cacheContainerKey, cache);
+                return cache;
+            }
+        }
+    }
+}
